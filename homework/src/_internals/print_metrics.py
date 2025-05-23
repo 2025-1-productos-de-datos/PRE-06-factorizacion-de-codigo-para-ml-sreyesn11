f7289b11ff6786e7876f484a2f0249bac9581f8d
Samuel Reyes <sreyesn@unal.edu.co>
2025-05-23 18:22:25 -0500

    se modulariza las funciones de los codigos

---
+++ homework/src/_internals/print_metrics.py
@@ -0,0 +1,6 @@
+def print_metrics(tittle, mse, mae, r2):
+    """Print metrics with a given title."""
+    print(f"\n{tittle}:")
+    print(f"  MSE: {mse}")
+    print(f"  MAE: {mae}")
+    print(f"  R2: {r2}")
